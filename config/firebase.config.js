28483e5c441dfc2fa1aa0953c2082d9f28718d74
nikhil-dex <jincossec@gmail.com>
2025-07-20 21:23:38 +0530

    boy

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -1,5 +1,5 @@
 const fbadmin = require("firebase-admin");
-const serviceCredentials = require("../project-1-e0088-firebase-adminsdk-fbsvc-f05a113667.json")
+const serviceCredentials = require("../etc/secret/project-1-e0088-firebase-adminsdk-fbsvc-f05a113667.json")
 
 fbadmin.initializeApp({
     credential: fbadmin.credential.cert(serviceCredentials),
